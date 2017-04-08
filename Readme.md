26a626d3262798170ad9e2122b03433158a0a05f
kt-uchiha <kanitshrm09@gmail.com>
2017-04-08 19:00:15 +0530

    First Commit

---
+++ Readme.md
@@ -0,0 +1,22 @@
+# Torrent CLI
+
+A super fast torrent client that works on your command line powered by node.js and webtorrent
+
+## Getting Started
+
+```
+git clone https://github.com/kanitsharma/Torrent-CLI.git
+```
+## Installing
+
+```
+cd Torrent-CLI
+```
+```
+npm install
+```
+##Running
+
+```
+npm start
+```
---
+++ index.js
@@ -0,0 +1,25 @@
+const webtorrent = require('webtorrent')
+const client  = new webtorrent()
+const rl = require('readline')
+const prompts = rl.createInterface(process.stdin,process.stdout)
+console.log("Welcome to Torrent CLI\n######################\n")
+setTimeout(() => {
+
+  prompts.question("Enter the torrent URL / Magnet URL / Filesystem path to a torrent file / Info hash : \n",(magnetURI) => {
+
+    client.add(magnetURI, { path: process.env.HOME + '/Downloads/TorrentCli' }, (torrent) => {
+      torrent.on('done',() => {
+        console.log('\nCheck Your Downloads Folder')
+        client.destroy(() => {
+          process.exit()
+        })
+      })
+    })
+    client.on('torrent',(torrent) => {
+      console.log("\nDownload has started\n");
+      setInterval(() => process.stdout.write('Download Speed: '+(client.downloadSpeed/1024).toFixed(2)+'KB/s'+'\tProgress: ' + (client.progress*100).toFixed(2) + '%' +'\r'),500)
+    })
+
+  })
+
+},1000)
